89d246ace0cb58dd7b834ff3a859a9f982083c92
mayur-driod <settymayurk@gmail.com>
2024-12-11 09:11:41 +0530

    Made some changes to software.jsx

--- src/Photography.jsx
+++ src/Photography.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import photo from './assets/DSC00045.JPG';
+import './SP.css'
 
 function Photography() {
   return (
     <>
       <h1>Photography</h1>
-      <img className='Mainpic' src={photo} alt='A beautiful photograph' />
+      <img className='Mainpic' src={photo} alt='malabar pit viper poised to strike' />
     </>
   );
 }
--- src/Software.jsx
+++ src/Software.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
-import photo from './assets/DSC00239.jpg'
+import photo from './assets/Laptop in classroom.jpg'
+import profile from './assets/IMG_3332~2.jpg'
+import './SP.css'
 
 function software() {
   return (
     <>
+    
+    {/* <img className='Mainpic' src={photo}/> */}
+    <div className='Maindiv' style={{backgroundImage:`url(${photo})`}}>
     <h1>Software</h1>
-    <img className='Mainpic' src={photo}/>
+      <div className='profileDiv'>
+        <h3>Hey I am Mayur</h3>
+        <img src={profile} alt="" />
+        <p>
+          I found my love for software and computers in the covid-19 pandemic
+        </p>
+      </div>
+    </div>
     </>
   )
 }
