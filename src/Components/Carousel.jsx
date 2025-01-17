8d4456e76d8511746196712ccf4a413c055584fa
mayur-driod <settymayurk@gmail.com>
2025-01-17 20:52:01 +0530

    resolved issues with image display

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,14 +1,17 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+import Slider from 'react-slick';
 
-// Here we have used react-icons package for the icons
+// Import react-slick styles
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-// And react-slick as our Carousel Lib
-import Slider from 'react-slick'
+// Import images
+import one from '../assets/Carousel/DSC00045.JPG';
+import two from '../assets/Carousel/DSC00239.jpg';
+import three from '../assets/Carousel/Kite.jpg';
 
-// Settings for the slider
 const settings = {
- 
   arrows: true,
   fade: true,
   infinite: true,
@@ -18,23 +21,24 @@ const settings = {
   slidesToShow: 1,
   slidesToScroll: 1,
   adaptiveHeight: true,
-}
+};
 
 export default function Carousel() {
-    const images= [
-        "https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?cs=srgb&dl=daylight-environment-forest-459225.jpg&fm=jpg",
-        "https://mediaim.expedia.com/destination/1/b2471b67db38b126e341925174ce6b1e.jpg?impolicy=fcrop&w=1040&h=580&q=mediumHigh",
-        "/src/assets/Carousel/DSC00045.JPG"
-    ]
-    return (
-        <div style={{width:"100%",height:"100%",margin:"auto"}}>
-            <Slider {...settings}>
-                {images.map((image, index) => (
-                    <Box key={index}>
-                        <img src={image} alt={`Photo ${index + 1}`}style={{width:"100%",height:"100%",borderRadius:"8px"}} />
-                    </Box>
-                ))}
-            </Slider>
-        </div>
-    )
+  const images = [one, two, three];
+
+  return (
+    <div style={{ width: "100%", maxWidth: "800px", margin: "auto", position: "relative" }}>
+      <Slider {...settings}>
+        {images.map((image, index) => (
+          <Box key={index}>
+            <img
+              src={image}
+              alt={`Photo ${index + 1}`}
+              style={{ width: "100%", height: "auto", borderRadius: "8px" }}
+            />
+          </Box>
+        ))}
+      </Slider>
+    </div>
+  );
 }
