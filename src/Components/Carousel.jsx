ab2e5eec2dc96b39bfff628572103e339ee66e09
mayur-driod <settymayurk@gmail.com>
2025-01-17 21:11:07 +0530

    added multiple photos and finetuned...

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -7,9 +7,27 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 // Import images
-import one from '../assets/Carousel/DSC00045.JPG';
-import two from '../assets/Carousel/DSC00239.jpg';
-import three from '../assets/Carousel/Kite.jpg';
+import one from '../assets/Carousel/1.jpg';
+import two from '../assets/Carousel/2.jpg';
+import three from '../assets/Carousel/3.jpg';
+import four from '../assets/Carousel/4.jpg';
+import five from '../assets/Carousel/5.jpg';
+import six from '../assets/Carousel/6.jpg';
+import seven from '../assets/Carousel/7.jpg';
+import eight from '../assets/Carousel/8.jpg';
+import nine from '../assets/Carousel/9.jpg';
+import ten from '../assets/Carousel/10.jpg';
+import eleven from '../assets/Carousel/11.jpg';
+import twelve from '../assets/Carousel/12.jpg';
+import thirteen from '../assets/Carousel/13.jpg';
+import fourteen from '../assets/Carousel/14.jpg';
+import fifteen from '../assets/Carousel/15.jpg';
+import sixteen from '../assets/Carousel/16.jpg';
+import seventeen from '../assets/Carousel/17.jpg';
+import eighteen from '../assets/Carousel/18.jpg';
+import ninteen from '../assets/Carousel/DSC00045.JPG';
+import twenty from '../assets/Carousel/DSC00239.jpg';
+import twentyone from '../assets/Carousel/Kite.jpg';
 
 const settings = {
   arrows: true,
@@ -24,10 +42,10 @@ const settings = {
 };
 
 export default function Carousel() {
-  const images = [one, two, three];
+  const images = [one, two, three, four,five,six,seven,eight,nine,ten,eleven,twelve,thirteen,fourteen,fifteen,sixteen,seventeen,eighteen,ninteen,twenty,twentyone];
 
   return (
-    <div style={{ width: "100%", maxWidth: "800px", margin: "auto", position: "relative" }}>
+    <div style={{ width: "100%", maxWidth: "70vw", margin: "auto", position: "relative" }}>
       <Slider {...settings}>
         {images.map((image, index) => (
           <Box key={index}>
