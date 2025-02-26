0837f7221b264d8cd0d0cf261b1b7aa363bc69d0
mayur-driod <settymayurk@gmail.com>
2025-02-26 11:47:08 +0530

    Software page cleaned up

--- src/Software.jsx
+++ src/Software.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import photo from './assets/CS Background 2.0.png';
+import photo from './assets/file cover - 1.png';
 import Profilecard from './SoftwareComp/Profilecard';
 import Skills from './SoftwareComp/Skills';
 import './SP.css';
 
 function Software() {
   return (
-    <div className='Maindiv' style={{ backgroundImage: `url(${photo})` }}>
-      <h1>Software</h1>
+    <div className='Maindiv' 
+    style={{ backgroundImage: `url(${photo})` }}
+    >
       <div className="content-container">
         <Profilecard />
         <Skills />
